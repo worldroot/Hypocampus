45e7625efae74751647ae01b8a44ce1066b777bf
Mehdi Behira <mehdibehira@gmail.com>
2020-02-13 00:16:09 +0100

    fix

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -18,72 +18,10 @@ class User extends BaseUser
      */
     protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="firstname", type="string", length=255)
-     */
-    private $firstname;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="lastname", type="string", length=255)
-     */
-    private $lastname;
-
     public function __construct()
     {
         parent::__construct();
         // your own logic
-    }
-
-    /**
-     * Set firstname
-     *
-     * @param string $str
-     *
-     * @return User
-     */
-    public function setFisrtname($str)
-    {
-        $this->firstname = $str;
-
-        return $this;
-    }
-
-    /**
-     * Get firstname
-     *
-     * @return string
-     */
-    public function getFirstname()
-    {
-        return $this->firstname;
-    }
-
-    /**
-     * Set lastname
-     *
-     * @param string $str
-     *
-     * @return User
-     */
-    public function setLastname($str)
-    {
-        $this->lastname = $str;
-
-        return $this;
-    }
-
-    /**
-     * Get lastname
-     *
-     * @return string
-     */
-    public function getLastname()
-    {
-        return $this->lastname;
     }
 
 
