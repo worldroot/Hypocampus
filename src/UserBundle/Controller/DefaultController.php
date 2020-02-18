ba920c3ea5ff05f4071563ed740e82ea2d88495a
Houcem Chettaoui <houcem.chett@gmail.com>
2020-02-18 17:13:07 +0100

    fos user 2.0
    ya 3atik 3asba ya fox

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -6,16 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route ("/")
-     */
+
     public function indexAction()
     {
-        return $this->render('@User/Security/login.html.twig');
+        return $this->render('@User/Default/index.html.twig');
     }
-    /**
-     * @Route ("/Register")
-     */
+
     public function registerAction()
     {
         return $this->render('@User/Registration/register.html.twig');
