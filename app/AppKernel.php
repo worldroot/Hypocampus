a63d8aacd7703b0831d3798a2c88ed7a4dfb93ec
Houcem Chettaoui <houcem.chett@gmail.com>
2020-02-20 11:11:47 +0100

    l validation thenya

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new SubscriptionBundle\SubscriptionBundle(),
+            new EntrepriseBundle\EntrepriseBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
